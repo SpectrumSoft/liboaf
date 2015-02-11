bbc5572863f20cb1778b02825efc8ab39a2999f7
Sergey N. Yatskevich <syatskevich@gmail.com>
2015-02-11 11:37:01 +0300

    Intermediate commit N1

--- idl/ITextDocument.h
+++ idl/ITextDocument.h
@@ -17,63 +17,10 @@
 namespace OAF
 {
 	/**
-	 * @brief Текстовый документ
-	 */
-	struct ITextDocument : virtual public OAF::IInterface
-	{
-		/**
-		 * @brief Аннотация документа, или его preview-версия (к примеру полезно для тултипов)
-		 */
-		virtual QString getDocumentAnnotation () = 0;
-
-		/**
-		 * @brief Проверить наличие текста в документе
-		 */
-		virtual bool isEmptyDocument () = 0;
-
-		/**
-		 * @brief Очистить документ
-		 */
-		virtual void clear () = 0;
-	};
-
-	/**
-	 * @brief Уведомление об изменении содержимого текстового документа
+	 * @brief Поиск дочерних компонентов в дереве владельца
 	 *
-	 * Используется в реализациях, которые предполагают рассылку уведомлений о изменениях своего
-	 * состояния. Такие объекты должны также поддерживать интерфейс OAF::INotifySource.
+	 * @obsoleted
 	 */
-	struct ITextDocumentNotify : virtual public OAF::IInterface
-	{
-		Q_DISABLE_COPY(ITextDocumentNotify)
-
-		/**
-		 * @brief Изменённый документ
-		 *
-		 * Данный параметр может не задаваться при рассылке уведомлений. Это зависит
-		 * от их источника.
-		 */
-		OAF::IInterface* const object;
-
-		/**
-		 * @brief Подпись объекта, частью которого является данный документ
-		 *
-		 * Эта подпись нужна для того, чтобы отслеживать изменение объектов, которые
-		 * включают в себя документы.
-		 */
-		OAF::IInterface* sign;
-
-		/**
-		 * @brief Конструктор уведомления
-		 */
-		ITextDocumentNotify (OAF::IInterface* _object, OAF::IInterface* _sign = NULL) :
-			object (_object), sign (_sign)
-		{}
-	};
-
-	/**
-	  * @brief Поиск дочерних компонентов в дереве владельца
-	  */
 	struct ITextDocumentResolver : virtual public OAF::IInterface
 	{
 		/**
@@ -105,6 +52,165 @@ namespace OAF
 		 * @brief Поиск компонента по его идентификатору
 		 */
 		virtual OAF::URef<OAF::IUnknown> findComponent (const QString& _id) const = 0;
+	};
+
+	/**
+	 * @brief Разрешение ссылок на внешние объекты в документах
+	 *
+	 * Объекты, которые возвращаются должны поддерживать интерфейсы:
+	 *     * OAF::IPropertyBag - для генерации ссылок на объекты (должны быть
+	 *       определены атрибуты id, uid и name);
+	 *     * OAF::IExportable - для включения содержания объектов в документы.
+	 */
+	struct ITextDocumentRefResolver : virtual public OAF::IInterface
+	{
+		/**
+		 * @brief Поиск объекта по его идентификатору
+		 */
+		virtual OAF::URef<OAF::IUnknown> refResolve (const QString& _id) const = 0;
+	};
+
+	/**
+	 * @brief Определение словарных терминов и получение их описаний
+	 */
+	struct ITextDocumentTermResolver : virtual public OAF::IInterface
+	{
+		/**
+		 * @brief Время последнего обновления базы терминов в формате time_t
+		 */
+		virtual uint termsUpdatedAt () = 0;
+
+		/**
+		 * @brief Заданное слово распознано как словарный термин
+		 */
+		virtual bool isTerm (const QString& _term) = 0;
+
+		/**
+		 * @brief Получить набор идентификаторов словарных статей для
+		 *        заданного термина
+		 *
+		 * При специальном значении @a _term @@referred возвращается список
+		 * используемых терминов.
+		 */
+		virtual std::size_t enumTermArticles (QStringList& _out, const QString& _term) = 0;
+
+		/**
+		 * @brief Получить словарную статью по её идентификатору
+		 *
+		 * Словарная статья должна реализовать интерфейс OAF::IPropertyBag с атрибутами:
+		 *     * id          - идентификатор статьи (QUuid);
+		 *     * name        - термин, который описывается в статье;
+		 *     * description - HTML-аннотация статьи.
+		 *
+		 * Словарная статья должна реализовать интерфейс OAF::IObjectCollection, через
+		 * который должен быть доступен объект с интерфейсами OAF::ITextDocument и
+		 * OAF::IExportable и идентификатором description, который содержит текст статьи.
+		 */
+		virtual OAF::URef<OAF::IUnknown> getTermArticle (const QString& _id) = 0;
+
+		/**
+		 * @brief Получить словарь для заданной словарной статьи
+		 *
+		 * Словарь должен реализовать интерфейс OAF::IPropertyBag с атрибутами:
+		 *     * id   - идентификатор словаря (QUuid);
+		 *     * uid  - пользовательский идентификатор словаря (QString)
+		 *     * name - наименование словаря (QString).
+		 */
+		virtual OAF::URef<OAF::IUnknown> getTermDictionary (const QString& _id) = 0;
+	};
+
+	/**
+	 * @brief Текстовый документ
+	 */
+	struct ITextDocument : virtual public OAF::IInterface
+	{
+		/**
+		 * @brief Установить объект, который должен разрешать ссылки
+		 *        на внешние объекты в документе
+		 */
+		virtual void setRefResolver (ITextDocumentRefResolver* _rr) = 0;
+
+		/**
+		 * @brief Установить объект, который должен разрешать ссылки
+		 *        на термины в документе
+		 */
+		virtual void setTermResolver (ITextDocumentTermResolver* _tr) = 0;
+
+		/**
+		 * @brief Проверка возможности добавления ссылок на внешние объекты в документ
+		 */
+		virtual bool canAddReferences () const = 0;
+
+		/**
+		 * @brief Добавление ссылки на внешний объект в документ
+		 *
+		 * FIXME: в случае когда представление отделено от документа для вставки
+		 *        ссылки необходимо задать место документа, куда её вставлять.
+		 *
+		 * @param[in] _id идентификатор внешнего объекта
+		 * @param[in] _collapse признак того, что при экспорте вместо объекта
+		 *                      должна быть вставлена ссылка на него
+		 * @param[in] _editable признак того, что объект может быть отредактирован
+		 *                      в месте вставки
+		 */
+		virtual void addReference (const QUuid& _id, bool _collapse = false, bool _editable = true) = 0;
+
+		/**
+		 * @brief Проверка возможности добавления ссылок на словари в документ
+		 */
+		virtual bool canAddDictionaries () const = 0;
+
+		/**
+		 * @brief Добавление ссылок на словари в документ
+		 */
+		virtual void addDictionaries (const QList<QUuid>& _ids, int _includes) = 0;
+
+		/**
+		 * @brief Аннотация документа, или его preview-версия (к примеру полезно для тултипов)
+		 */
+		virtual QString getDocumentAnnotation () = 0;
+
+		/**
+		 * @brief Проверить наличие текста в документе
+		 */
+		virtual bool isEmptyDocument () = 0;
+
+		/**
+		 * @brief Очистить документ
+		 */
+		virtual void clear () = 0;
+	};
+
+	/**
+	 * @brief Уведомление об изменении содержимого текстового документа
+	 *
+	 * Используется в реализациях, которые предполагают рассылку уведомлений о изменениях своего
+	 * состояния. Такие объекты должны также поддерживать интерфейс OAF::INotifySource.
+	 */
+	struct ITextDocumentNotify : virtual public OAF::IInterface
+	{
+		/**
+		 * @brief Изменённый документ
+		 *
+		 * Данный параметр может не задаваться при рассылке уведомлений. Это зависит
+		 * от их источника.
+		 */
+		OAF::IInterface* const object;
+
+		/**
+		 * @brief Подпись объекта, частью которого является данный документ
+		 *
+		 * Эта подпись нужна для того, чтобы отслеживать изменение объектов, которые
+		 * включают в себя документы.
+		 */
+		OAF::IInterface* sign;
+
+		/**
+		 * @brief Конструктор уведомления
+		 */
+		ITextDocumentNotify (OAF::IInterface* _object, OAF::IInterface* _sign = NULL) :
+			object (_object), sign (_sign)
+		{}
 	};
 }
 
